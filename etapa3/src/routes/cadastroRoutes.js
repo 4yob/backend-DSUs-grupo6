19ae7f5eabf723997838934bad417eb4a17a790e
Evelyn <evelynoliveira.tds1.senai@gmail.com>
2024-11-28 11:37:11 -0300

    feat: update cadastroRouts

--- etapa3/src/routes/cadastroRoutes.js
+++ etapa3/src/routes/cadastroRoutes.js
@@ -1,28 +1,28 @@
 import { Router } from "express";
-import Jogo from "../../../etapa2/src/models/Jogo.js";
 
 const cadastroRoutes = Router();
 
-const jogoLista = new Jogo();
 let alunos = []
 
-
+/*Rota para cadastrar um aluno*/
 cadastroRoutes.post("/adicionar", (req, res) => {
-    const {name, idade, turma} = req.body
+    const { name, idade, turma } = req.body
 
     // Validação para campos obrigatórios
-    if(!name || !turma){
+    if (!name || !turma) {
         return res.status(400).send({
-            message: "Os campos nome e turma são obrigatórios"})
+            message: "Os campos nome e turma são obrigatórios"
+        })
     }
 
-    if(!Number.isInteger(idade)){
+    if (!Number.isInteger(idade)) {
         return res.status(400).send({
-            message: "Por favor digite um número inteiro"})
+            message: "Por favor digite um número inteiro"
+        })
     }
 
     const novoAluno = {
-        id: Number(Math.floor(Math.random()* 999)+ 1),
+        id: Number(Math.floor(Math.random() * 999) + 1),
         name,
         idade,
         turma
@@ -30,27 +30,30 @@ cadastroRoutes.post("/adicionar", (req, res) => {
 
     alunos.push(novoAluno)
     return res.status(201).send({
-        message: "Aluno cadastrado com sucesso!", 
+        message: "Aluno cadastrado com sucesso!",
         novoAluno
     })
 });
 
+
+/*Rota para listar todos os alunos*/
 cadastroRoutes.get("/listar", (req, res) => {
     return res.status(200).send(alunos);
 });
 
+/*Rota para atualizar um aluno pelo id*/
 cadastroRoutes.put("/atualizar:id", (req, res) => {
-    const{ id } =req.params
+    const { id } = req.params
 
     const aluno = alunos.find((student) => student.id === Number(id))
 
-    if(!aluno){
+    if (!aluno) {
         return res.status(404).send({
             message: "Aluno não encontrado!"
         })
     }
 
-    const {name, idade, turma} = req.body
+    const { name, idade, turma } = req.body
 
     aluno.name = name
     aluno.idade = idade
@@ -62,12 +65,13 @@ cadastroRoutes.put("/atualizar:id", (req, res) => {
     })
 });
 
+/*Rota para remover um aluno pelo id*/
 cadastroRoutes.delete("/remover:id", (req, res) => {
     const { id } = req.params
 
     const aluno = alunos.find((student) => student.id === Number(id))
 
-    if(!aluno) {
+    if (!aluno) {
         return res.status(404).send({
             message: "Aluno não encontrado!"
         })
