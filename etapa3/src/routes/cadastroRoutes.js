b940df5cfc0ceeb0ab102b8a7b584f09556c7147
Laurinhaaa <158209993+imlaurinhaaa@users.noreply.github.com>
2024-11-28 10:20:58 -0300

    feat: create method post

--- etapa3/src/routes/cadastroRoutes.js
+++ etapa3/src/routes/cadastroRoutes.js
@@ -2,8 +2,34 @@ import { Router } from "express";
 
 const cadastroRoutes = Router();
 
-cadastroRoutes.post("/adicionar", (req, res) => {
+let alunos = []
 
+cadastroRoutes.post("/adicionar", (req, res) => {
+    const {name, idade, turma} = req.body
+
+    // Validação para campos obrigatórios
+    if(!name || !turma){
+        return res.status(400).send({
+            message: "Os campos nome e turma são obrigatórios"})
+    }
+
+    if(!Number.isInteger(idade)){
+        return res.status(400).send({
+            message: "Por favor digite um número inteiro"})
+    }
+
+    const novoAluno = {
+        id: Number(Math.floor(Math.random()* 999)+ 1),
+        name,
+        idade,
+        turma
+    }
+
+    alunos.push(novoAluno)
+    return res.status(201).send({
+        message: "Aluno cadastrado com sucesso!", 
+        novoAluno
+    })
 });
 
 cadastroRoutes.get("/listar", (req, res) => {
